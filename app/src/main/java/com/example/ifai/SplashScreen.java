a8f1518d122505fcbc353766e96bf46272369588
Farooq <mullafarooq4@gmail.com>
2020-11-02 10:13:44 +0530

    repo initial commit

---
+++ app/src/main/java/com/example/ifai/SplashScreen.java
@@ -0,0 +1,17 @@
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ifai.MainActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
